33ec080ee68cb08e9c546bc8304adbc5711c00db
Paulo Tintaya <pau.tintaya.code@gamil.com>
2023-07-12 18:08:12 -0400

    APIView implemented for core app

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+@admin.register(Contact)
+class ContacatAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'email')
--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.urls import path
 from django.contrib import admin
+from core import views as core_views
 from rest_framework import routers
+
 
 router = routers.DefaultRouter()
 
@@ -23,4 +25,5 @@ urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
+    path('contact/', core_views.ContactAPIView.as_view()),
 ]
